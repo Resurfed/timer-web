e16d3b318cfb94497f2872d74ba9da625e59e459
Max v <20483150+mvonsm@users.noreply.github.com>
2017-12-23 02:06:44 -0500

    fixed project name

--- timerweb/settings.py
+++ timerweb/settings.py
@@ -44,7 +44,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'timer.urls'
+ROOT_URLCONF = 'timerweb.urls'
 
 TEMPLATES = [
     {
@@ -62,7 +62,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'timer.wsgi.application'
+WSGI_APPLICATION = 'timerweb.wsgi.application'
 
 
 # Database
--- timerweb/wsgi.py
+++ timerweb/wsgi.py
@@ -2,6 +2,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "timer.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "timerweb.settings")
 
 application = get_wsgi_application()
