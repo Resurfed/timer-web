be1c11de3de6d1df7cff7f381976611a08330453
Max v <20483150+mvonsm@users.noreply.github.com>
2017-12-28 00:24:23 -0500

    I dont think this will work

--- timerweb/settings.py
+++ timerweb/settings.py
@@ -50,11 +50,7 @@ ROOT_URLCONF = 'timerweb.urls'
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
-
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+STATIC_URL = '/staticfiles/'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
