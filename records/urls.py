ac8d3881af630094c780b493d5fd938dee5cc49f
Max v <20483150+mvonsm@users.noreply.github.com>
2018-01-02 14:04:55 -0500

    forgot something

--- records/urls.py
+++ records/urls.py
@@ -3,10 +3,10 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from records import views
 
 urlpatterns = [
-	path('', views.TimeList.as_view()),
-	path('<int:pk>/', views.TimeDetail.as_view()),
-	path('servers/', views.ServerList.as_view())
-	path('servers/<int:pk>', views.ServerDetail.as_view())
+    path('', views.TimeList.as_view()),
+    path('<int:pk>/', views.TimeDetail.as_view()),
+    path('servers/', views.ServerList.as_view()),
+    path('servers/<int:pk>', views.ServerDetail.as_view())
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
