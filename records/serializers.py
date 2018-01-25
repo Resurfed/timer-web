26d022bb7d9f5732c6d9e46f21b37524cb108050
Max v <20483150+mvonsm@users.noreply.github.com>
2018-01-25 13:45:05 -0500

    Deleted mixin and moved rank fix to serializer.

--- records/serializers.py
+++ records/serializers.py
@@ -5,12 +5,16 @@ from .models import Time
 class TimeSerializer(serializers.ModelSerializer):
     player_name = serializers.SerializerMethodField()
     map_name = serializers.SerializerMethodField()
+    rank = serializers.SerializerMethodField()
 
     def get_player_name(self, obj):
         return obj.player.name
 
     def get_map_name(self, obj):
         return obj.map.name
+
+    def get_rank(self, obj):
+        return obj.actual_rank()
 
     class Meta:
         model = Time
