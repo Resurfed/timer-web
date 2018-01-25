26db7a2b3036c420037c217d56df36dd0282f98e
Max v <20483150+mvonsm@users.noreply.github.com>
2018-01-25 13:21:04 -0500

    Show specific information on time request

--- records/serializers.py
+++ records/serializers.py
@@ -3,6 +3,15 @@ from .models import Time
 
 
 class TimeSerializer(serializers.ModelSerializer):
+    player_name = serializers.SerializerMethodField()
+    map_name = serializers.SerializerMethodField()
+
+    def get_player_name(self, obj):
+        return obj.player.name
+
+    def get_map_name(self, obj):
+        return obj.map.name
+
     class Meta:
         model = Time
         fields = '__all__'
