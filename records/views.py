fb86843900e36c6d54814dd52eadeb63538562ed
Max v <20483150+mvonsm@users.noreply.github.com>
2018-01-25 13:45:05 -0500

    Deleted mixin and moved rank fix to serializer.

--- records/views.py
+++ records/views.py
@@ -3,16 +3,15 @@ from rest_framework import generics
 from .models import Time
 from .serializers import TimeSerializer
 from .filters import TimeFilter
-from .mixins import TimeRetrieveModelMixin, TimeListModelMixin
 
 
-class TimeList(TimeListModelMixin, generics.ListCreateAPIView):
+class TimeList(generics.ListCreateAPIView):
     queryset = Time.objects.all()
     serializer_class = TimeSerializer
     filter_class = TimeFilter
 
 
-class TimeDetail(TimeRetrieveModelMixin, generics.RetrieveUpdateDestroyAPIView):
+class TimeDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Time.objects.all()
     serializer_class = TimeSerializer
 
