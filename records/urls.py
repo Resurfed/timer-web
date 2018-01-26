a20a1a382fbb2ad53f31e84ee581fb9b7c58725c
Max v <20483150+mvonsm@users.noreply.github.com>
2018-01-25 20:07:13 -0500

    removed slash

--- records/urls.py
+++ records/urls.py
@@ -3,7 +3,7 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from records import views
 
 urlpatterns = [
-    path('/', views.TimeList.as_view()),
+    path('', views.TimeList.as_view()),
     path('<int:pk>/', views.TimeDetail.as_view()),
 
 ]
